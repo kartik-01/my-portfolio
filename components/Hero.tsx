cbc5645b8b48ebb4f9133c814a41092b2e582697
kartik-chindarkar <kartik.chindarkar@sjsu.edu>
2024-10-12 23:30:27 -0700

    header update

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -48,7 +48,7 @@ const Hero = () => {
            *  change md:text-6xl, add more responsive code
            */}
           <TextGenerateEffect
-            words="Transforming Concepts into Seamless User Experiences"
+            words="Hello, welcome to my World!"
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
           />
 
